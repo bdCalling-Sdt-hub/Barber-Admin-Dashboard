7d1108cc49fff29d8576a261c99cbec10f5df22b
alshakibeelahi <alshakibeelahi@gmail.com>
2023-09-23 18:30:08 +0600

    Message part completed

--- src/Pages/Dashboard/Message/Message.jsx
+++ src/Pages/Dashboard/Message/Message.jsx
@@ -15,8 +15,42 @@ import { AiOutlineCloudUpload } from "react-icons/ai";
 //import DatePicker from "react-multi-date-picker";
 import styleForm from "./Message.module.css";
 import { UserOutlined, SearchOutlined } from '@ant-design/icons';
+import { Link } from "react-router-dom";
 const { Option } = Select;
 const { Dragger } = Upload;
+
+const data = [
+  {
+    id: 1234,
+    sender: "John Doe",
+    message: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo con"
+  },
+  {
+    id: 1234,
+    sender: "John Doe",
+    message: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo con"
+  },
+  {
+    id: 1234,
+    sender: "John Doe",
+    message: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo con"
+  },
+  {
+    id: 1234,
+    sender: "John Doe",
+    message: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo con"
+  },
+  {
+    id: 1234,
+    sender: "John Doe",
+    message: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo con"
+  },
+  {
+    id: 1234,
+    sender: "John Doe",
+    message: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo con"
+  },
+]
 
 const props = {
   name: "file",
@@ -79,30 +113,92 @@ const Message = () => {
     <div style={{ padding: "0 60px" }}>
       <div>
         <form>
-          <div>
+          <div style={{ marginBottom: "50px" }}>
             {/* <input className={`${styleForm.sendAllMessageInputBox}`} type='text' placeholder="Write message"/> */}
             <TextArea rows={8} className={styleForm.sendMessage} style={{ background: '#364153', color: '#FFF' }} placeholder="Write Message" />
             <div>
               <input className={`${styleForm.sendAllButton}`} type="submit" value={'Send to All'} />
             </div>
           </div>
-          <Row style={{ marginBottom: "30px" }}>
+          <Row style={{ marginBottom: "50px" }}>
             <h2 className={styleForm.h2Design}>
               Search Messages
             </h2>
-            <Col lg={{ span: 24 }}>
+            <Col lg={{ span: 24 }} style={{ marginTop: "30px" }}>
               <div className='' style={{ display: "flex", gap: "15px" }}>
                 <Input className={styleForm.searchMessageInput} size="large" placeholder="Search by name/ID" prefix={<SearchOutlined style={{ color: "#CFCFD0" }} />} />
-                <Button style={{ height: "50px", width: "300px", backgroundColor: "#F66D0F", color: "#fff", fontSize: "20px" }}>Search</Button>
+                <Button className={styleForm.searchMessageInput} style={{ height: "50px", width: "300px", backgroundColor: "#F66D0F", color: "#fff", fontSize: "20px" }}>Search</Button>
               </div>
             </Col>
           </Row>
           <Row>
-            <h2>Recent Messages</h2>
-            <div>
-              <input type="text" placeholder="search by name/ID" />
-              <input type="submit" value={'Search'} />
-            </div>
+            <h2 className={styleForm.h2Design}>
+              Recent Messages
+            </h2>
+            {
+              Array.isArray(data) ?
+                (<>
+                  {
+                    data.map((item, index) => {
+
+                      <div className={styleForm.recentMessage}>
+                        <div className={styleForm.recentMessageInfo}>
+                          <img className={styleForm.recentMessageImage} height={"40px"} width={"40px"} src={`${item.image}`} />
+                          <div className={styleForm.recentMessageSender}>{item.sender}</div>
+
+                        </div>
+                        <div className={styleForm.recentMessageDetails}>{item.message}</div>
+                        <svg className={styleForm.recentMessageIcon} width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <rect width="60" height="60" rx="8" fill="#F66D0F" />
+                          <path d="M35.98 28.79V32.79C35.98 33.05 35.97 33.3 35.94 33.54C35.71 36.24 34.12 37.58 31.19 37.58H30.79C30.54 37.58 30.3 37.7 30.15 37.9L28.95 39.5C28.42 40.21 27.56 40.21 27.03 39.5L25.83 37.9C25.7 37.73 25.41 37.58 25.19 37.58H24.79C21.6 37.58 20 36.79 20 32.79V28.79C20 25.86 21.35 24.27 24.04 24.04C24.28 24.01 24.53 24 24.79 24H31.19C34.38 24 35.98 25.6 35.98 28.79Z" stroke="white" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                          <path d="M39.98 24.79V28.79C39.98 31.73 38.63 33.31 35.94 33.54C35.97 33.3 35.98 33.05 35.98 32.79V28.79C35.98 25.6 34.38 24 31.19 24H24.79C24.53 24 24.28 24.01 24.04 24.04C24.27 21.35 25.86 20 28.79 20H35.19C38.38 20 39.98 21.6 39.98 24.79Z" stroke="white" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                          <path d="M31.4955 31.25H31.5045" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
+                          <path d="M27.9955 31.25H28.0045" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
+                          <path d="M24.4955 31.25H24.5045" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                      </div>
+                      { console.log(item.image) }
+                    }
+                    )}
+                </>) : (<>
+                  <div className={styleForm.recentMessage}>
+                    No message found
+                  </div>
+                </>)
+            }
+            {
+              Array.isArray(data) ? (
+                <>
+                  {
+                    data.map((item, index) => (
+                      <div className={styleForm.recentMessage} key={index}>
+                        <div className={styleForm.recentMessageInfo}>
+                          <img className={styleForm.recentMessageImage} height={"40px"} width={"40px"} src='src\Images\Photo.png' />
+                          <div className={styleForm.recentMessageSender}>{item.sender}</div>
+                        </div>
+                        <div className={styleForm.recentMessageDetails}>{item.message}</div>
+                        <div className={styleForm.recentMessageIcon} >
+                          <Link to={`/message/${item.id}`}>
+                            <svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <rect width="60" height="60" rx="8" fill="#F66D0F" />
+                              <path d="M35.98 28.79V32.79C35.98 33.05 35.97 33.3 35.94 33.54C35.71 36.24 34.12 37.58 31.19 37.58H30.79C30.54 37.58 30.3 37.7 30.15 37.9L28.95 39.5C28.42 40.21 27.56 40.21 27.03 39.5L25.83 37.9C25.7 37.73 25.41 37.58 25.19 37.58H24.79C21.6 37.58 20 36.79 20 32.79V28.79C20 25.86 21.35 24.27 24.04 24.04C24.28 24.01 24.53 24 24.79 24H31.19C34.38 24 35.98 25.6 35.98 28.79Z" stroke="white" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                              <path d="M39.98 24.79V28.79C39.98 31.73 38.63 33.31 35.94 33.54C35.97 33.3 35.98 33.05 35.98 32.79V28.79C35.98 25.6 34.38 24 31.19 24H24.79C24.53 24 24.28 24.01 24.04 24.04C24.27 21.35 25.86 20 28.79 20H35.19C38.38 20 39.98 21.6 39.98 24.79Z" stroke="white" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                              <path d="M31.4955 31.25H31.5045" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
+                              <path d="M27.9955 31.25H28.0045" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
+                              <path d="M24.4955 31.25H24.5045" stroke="white" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                          </Link>
+                        </div>
+                      </div>
+                    ))
+                  }
+                </>
+              ) : (
+                <div className={styleForm.recentMessage}>
+                  No message found
+                </div>
+              )
+            }
           </Row>
         </form>
       </div>
